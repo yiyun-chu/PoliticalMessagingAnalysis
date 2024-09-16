4b06cb8ae05da705e905645d453bc2c0663647ed
yiyun-chu <yiyunchu@andrew.cmu.edu>
2024-09-16 17:41:08 -0400

    update path

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import json
 import os
 
 from vpn_automate import change_vpn, login_vpn
-save_dir = "after_submit_screenshot"
+save_dir = r"C:\Users\heinz_3\Dropbox\after_submit_screenshot"
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
     
@@ -99,11 +99,15 @@ for i in range(len(df['Url'])):
         pass
         
     if "gallegoforarizona" in url:
+        time.sleep(1)
         all_form_input[0].send_keys(df['email'][i])
+        time.sleep(1)
         all_form_input[1].send_keys("8755968766") 
+        time.sleep(1)
         all_form_input[2].send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//button[@type='submit']").click()
-        time.sleep(6)
+        time.sleep(3)
         
         username = df["email"][i].split("@")[0]
         formatted_url = url.replace("www.", "").split(".")[0].split("//")[1]
@@ -112,13 +116,21 @@ for i in range(len(df['Url'])):
         
         
     elif "karilake" in url:
+        time.sleep(1)
         all_form_input[0].send_keys("john")
+        time.sleep(1)
         all_form_input[1].send_keys("smit") 
+        time.sleep(1)
         all_form_input[2].send_keys("8755968767")
+        time.sleep(1)
         all_form_input[3].send_keys(df['email'][i])
-        all_form_input[4].send_keys("delhi")  
-        all_form_input[5].send_keys(int(df['zip'][i]))  
+        time.sleep(1)
+        all_form_input[4].send_keys("delhi")
+        time.sleep(1)  
+        all_form_input[5].send_keys(int(df['zip'][i]))
+        time.sleep(1)  
         submit = driver.find_element(By.XPATH,"//input[@type='submit']")
+        time.sleep(1)
         driver.execute_script("arguments[0].click();",submit)
         time.sleep(10)
         
@@ -130,8 +142,11 @@ for i in range(len(df['Url'])):
     elif "elizabethwarren" in url:
         time.sleep(6) 
         all_form_input[0].send_keys(df['email'][i])
+        time.sleep(1)
         all_form_input[1].send_keys(int(df['zip'][i]))
-        all_form_input[2].send_keys("8755968766") 
+        time.sleep(1)
+        all_form_input[2].send_keys("8755968766")
+        time.sleep(1) 
         driver.find_element(By.XPATH,"//button[@type='submit']").click()
         time.sleep(6)
         
@@ -143,8 +158,11 @@ for i in range(len(df['Url'])):
     elif "johndeatonforsenate" in url: 
         time.sleep(5)
         all_form_input[0].send_keys(df['First Name'][i])
-        all_form_input[1].send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        all_form_input[1].send_keys(df['Last Name'][i])
+        time.sleep(1) 
         all_form_input[2].send_keys(df['email'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -153,10 +171,14 @@ for i in range(len(df['Url'])):
         screenshot_path = os.path.join(save_dir, f'{username}_{formatted_url}.png')
         driver.save_screenshot(screenshot_path)            
         
-    elif "elissaslotkin.org" in url: 
+    elif "elissaslotkin.org" in url:
+        time.sleep(1) 
         all_form_input[0].send_keys(df['email'][i])
-        all_form_input[1].send_keys("8755968766") 
+        time.sleep(1)
+        all_form_input[1].send_keys("8755968766")
+        time.sleep(1) 
         all_form_input[2].send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//button[@type='submit']").click()
         time.sleep(6)
         
@@ -165,9 +187,12 @@ for i in range(len(df['Url'])):
         screenshot_path = os.path.join(save_dir, f'{username}_{formatted_url}.png')
         driver.save_screenshot(screenshot_path)            
         
-    elif "rogersforsenate" in url: 
+    elif "rogersforsenate" in url:
+        time.sleep(1) 
         all_form_input[0].send_keys(df['email'][i])
+        time.sleep(1)
         all_form_input[1].send_keys("8755968766")
+        time.sleep(1)
         all_form_input[2].send_keys(int(df['zip'][i]))
         driver.find_element(By.XPATH,"//button[@type='submit']").click()
         time.sleep(6)
@@ -176,11 +201,16 @@ for i in range(len(df['Url'])):
         screenshot_path = os.path.join(save_dir, f'{username}_{formatted_url}.png')
         driver.save_screenshot(screenshot_path)
             
-    elif "amyklobuchar" in  url: 
+    elif "amyklobuchar" in  url:
+        time.sleep(1) 
         all_form_input[0].send_keys(df['First Name'][i])
-        all_form_input[1].send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        all_form_input[1].send_keys(df['Last Name'][i])
+        time.sleep(1) 
         all_form_input[2].send_keys(int(df['zip'][i]))
-        all_form_input[3].send_keys(df['email'][i]) 
+        time.sleep(1)
+        all_form_input[3].send_keys(df['email'][i])
+        time.sleep(1) 
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -189,13 +219,20 @@ for i in range(len(df['Url'])):
         screenshot_path = os.path.join(save_dir, f'{username}_{formatted_url}.png')
         driver.save_screenshot(screenshot_path)            
         
-    elif "roycewhite.us" in  url: 
+    elif "roycewhite.us" in  url:
+        time.sleep(1) 
         all_form_input[8].send_keys(df['First Name'][i])
-        all_form_input[9].send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        all_form_input[9].send_keys(df['Last Name'][i])
+        time.sleep(1) 
         all_form_input[10].send_keys(df['email'][i])
+        time.sleep(1)
         all_form_input[11].send_keys("8755968766")  
+        time.sleep(1)
         input = form.find_element(By.XPATH,"//input[@placeholder='ZIP Code*']")
-        input.send_keys(int(df['zip'][i]))     
+        time.sleep(1)
+        input.send_keys(int(df['zip'][i]))
+        time.sleep(1)     
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -204,12 +241,18 @@ for i in range(len(df['Url'])):
         screenshot_path = os.path.join(save_dir, f'{username}_{formatted_url}.png')
         driver.save_screenshot(screenshot_path)   
                     
-    elif "lucaskunce" in  url: 
+    elif "lucaskunce" in  url:
+        time.sleep(1) 
         all_form_input[0].send_keys(df['First Name'][i])
-        all_form_input[1].send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        all_form_input[1].send_keys(df['Last Name'][i])
+        time.sleep(1) 
         all_form_input[2].send_keys(df['email'][i])
-        all_form_input[3].send_keys(int(df['zip'][i]))   
-        all_form_input[4].send_keys("8755968766")   
+        time.sleep(1)
+        all_form_input[3].send_keys(int(df['zip'][i]))
+        time.sleep(1)   
+        all_form_input[4].send_keys("8755968766")
+        time.sleep(1)   
         submit = driver.find_element(By.XPATH,"//button[@type='submit']")
         time.sleep(6)
         driver.execute_script("arguments[0].click();",submit)
@@ -220,16 +263,20 @@ for i in range(len(df['Url'])):
         screenshot_path = os.path.join(save_dir, f'{username}_{formatted_url}.png')
         driver.save_screenshot(screenshot_path)            
         
-    elif "joshhawley" in  url:       
+    elif "joshhawley" in  url:
+        time.sleep(1)       
         driver.find_element(By.XPATH,"//input[@placeholder='First Name']").send_keys(df['First Name'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Last Name']").send_keys(df['Last Name'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Email Address']").send_keys(df['email'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Zip Code']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Phone Number (optional)']").send_keys("8755968766")
         time.sleep(6)
         submit = driver.find_element(By.XPATH,"//input[@type='submit']")
         time.sleep(6)
-
         driver.execute_script("arguments[0].click();",submit)
         time.sleep(6)
         
@@ -238,10 +285,14 @@ for i in range(len(df['Url'])):
         screenshot_path = os.path.join(save_dir, f'{username}_{formatted_url}.png')
         driver.save_screenshot(screenshot_path)            
         
-    elif "jontester" in url:          
+    elif "jontester" in url:
+        time.sleep(1)          
         all_form_input[0].send_keys(df['email'][i])
-        all_form_input[1].send_keys("8755968766") 
+        time.sleep(1)
+        all_form_input[1].send_keys("8755968766")
+        time.sleep(1) 
         all_form_input[2].send_keys(int(df['zip'][i]))
+        time.sleep(1)
         submit = driver.find_element(By.XPATH,"//button[@type='submit']")
         time.sleep(6)
 
@@ -255,8 +306,11 @@ for i in range(len(df['Url'])):
         
     elif "timformt" in url:
         all_form_input[0].send_keys(df['First Name'][i])
-        all_form_input[1].send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        all_form_input[1].send_keys(df['Last Name'][i])
+        time.sleep(1) 
         all_form_input[2].send_keys(df['email'][i])
+        time.sleep(1)
         all_form_input[3].send_keys("8755968766")
         time.sleep(6)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
@@ -273,8 +327,11 @@ for i in range(len(df['Url'])):
         body.send_keys(Keys.ESCAPE)
         time.sleep(1)
         driver.find_element(By.XPATH,"//input[@name='EmailAddress']").send_keys(df['email'][i])
-        driver.find_element(By.XPATH,"//input[@name='MobilePhone']").send_keys("8755968766") 
+        time.sleep(1)
+        driver.find_element(By.XPATH,"//input[@name='MobilePhone']").send_keys("8755968766")
+        time.sleep(1) 
         driver.find_element(By.XPATH,"//input[@name='PostalCode']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -284,12 +341,18 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)            
         
     elif "secure.ngpvan.com" in url :
+        time.sleep(1)
         all_form_input[0].send_keys(df['First Name'][i])
-        all_form_input[1].send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        all_form_input[1].send_keys(df['Last Name'][i])
+        time.sleep(1) 
         all_form_input[2].clear()
+        time.sleep(1)
         all_form_input[2].send_keys(df['email'][i])
+        time.sleep(1)
         all_form_input[3].clear()
         all_form_input[3].send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -298,11 +361,16 @@ for i in range(len(df['Url'])):
         screenshot_path = os.path.join(save_dir, f'{username}_{formatted_url}.png')
         driver.save_screenshot(screenshot_path)       
 
-    elif "captainsambrown" in url:   
+    elif "captainsambrown" in url:
+        time.sleep(1)   
         all_form_input[0].send_keys(df['email'][i])
-        all_form_input[1].send_keys("8755968766") 
+        time.sleep(1)
+        all_form_input[1].send_keys("8755968766")
+        time.sleep(1) 
         all_form_input[2].send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='checkbox']").click()
+        time.sleep(1)
         driver.find_element(By.XPATH,"//button[@type='submit']").click()
         time.sleep(6)
         
@@ -312,9 +380,13 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)            
         
     elif "mikesapraiconeforsenate" in url:
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Cell Phone']").send_keys("8755968766")
-        driver.find_element(By.XPATH,"//input[@placeholder='Email Address']").send_keys(df['email'][i]) 
+        time.sleep(1)
+        driver.find_element(By.XPATH,"//input[@placeholder='Email Address']").send_keys(df['email'][i])
+        time.sleep(1) 
         driver.find_element(By.XPATH,"//input[@placeholder='Zip Code']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//button[@type='submit']").click()
         time.sleep(6)
 
@@ -326,13 +398,19 @@ for i in range(len(df['Url'])):
     elif  "sherrodbrown" in url:
         time.sleep(3)
         body = driver.find_element(By.TAG_NAME, 'body')
+        time.sleep(1)
         body.send_keys(Keys.ESCAPE)
         time.sleep(1)
         driver.find_element(By.XPATH,"//input[@name='FirstName']").send_keys(df['First Name'][i])
-        driver.find_element(By.XPATH,"//input[@name='LastName']").send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        driver.find_element(By.XPATH,"//input[@name='LastName']").send_keys(df['Last Name'][i])
+        time.sleep(1) 
         driver.find_element(By.XPATH,"//input[@name='EmailAddress']").send_keys(df['email'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@name='MobilePhone']").send_keys("8755968766")
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@name='PostalCode']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -360,11 +438,17 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)            
         
     elif "bobcasey" in url:
+        time.sleep(1)
         all_form_input[0].send_keys(df['First Name'][i])
-        all_form_input[1].send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        all_form_input[1].send_keys(df['Last Name'][i])
+        time.sleep(1) 
         all_form_input[2].send_keys(df['email'][i])
+        time.sleep(1)
         all_form_input[3].send_keys(int(df['zip'][i]))
+        time.sleep(1)
         all_form_input[4].send_keys("8755968766")
+        time.sleep(1)
         submit = driver.find_element(By.XPATH,"//button[@type='submit']")
         driver.execute_script("arguments[0].click();", submit)
         time.sleep(6)
@@ -374,11 +458,17 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)
                     
     elif "davemccormickpa" in url:
+        time.sleep(1)
         all_form_input[0].send_keys(df['First Name'][i])
-        all_form_input[1].send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        all_form_input[1].send_keys(df['Last Name'][i])
+        time.sleep(1) 
         all_form_input[2].send_keys("8755968766")
+        time.sleep(1)
         all_form_input[3].send_keys(int(df['zip'][i]))
+        time.sleep(1)
         all_form_input[4].send_keys(df['email'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -388,10 +478,13 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)            
         
     elif "votegloriajohnson"  in url :
-        
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Email*']").send_keys(df['email'][i])
-        driver.find_element(By.XPATH,"//input[@name='MobilePhone']").send_keys("8755968766") 
+        time.sleep(1)
+        driver.find_element(By.XPATH,"//input[@name='MobilePhone']").send_keys("8755968766")
+        time.sleep(1) 
         driver.find_element(By.XPATH,"//input[@placeholder='ZIP Code*']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -401,8 +494,11 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)            
         
     elif  "marshablackburn" in url:
-        driver.find_element(By.XPATH,"//input[@placeholder='Email Address']").send_keys(df['email'][i]) 
+        time.sleep(1)
+        driver.find_element(By.XPATH,"//input[@placeholder='Email Address']").send_keys(df['email'][i])
+        time.sleep(1) 
         driver.find_element(By.XPATH,"//input[@placeholder='Zip Code']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -412,9 +508,13 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)            
         
     elif "colinallred" in url:
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='US Zip Code']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Email Address']").send_keys(df['email'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Phone']").send_keys("8755968766")
+        time.sleep(1)
         driver.find_element(By.XPATH,"//button[@type='submit']").click()
         time.sleep(6)
         
@@ -424,8 +524,11 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)            
         
     elif "erichovde" in url :
+        time.sleep(1)
         all_form_input[0].send_keys(df['email'][i])
+        time.sleep(1)
         all_form_input[1].send_keys(int(df['zip'][i])) 
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -441,9 +544,13 @@ for i in range(len(df['Url'])):
         except:
             pass
         driver.find_element(By.XPATH,"//input[@id='form-first_name']").send_keys(df['First Name'][i])
-        driver.find_element(By.XPATH,"//input[@id='form-last_name']").send_keys(df['Last Name'][i]) 
+        time.sleep(1)
+        driver.find_element(By.XPATH,"//input[@id='form-last_name']").send_keys(df['Last Name'][i])
+        time.sleep(1) 
         driver.find_element(By.XPATH,"//input[@id='form-email']").send_keys(df['email'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@id='form-zip_code']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(6)
         
@@ -453,9 +560,13 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)
         
     elif "tedcruz" in url:
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Email address*']").send_keys(df['email'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='ZIP Code*']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Phone']").send_keys("8755968788")
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@type='submit']").click()
         time.sleep(12)
         
@@ -485,8 +596,11 @@ for i in range(len(df['Url'])):
         driver.save_screenshot(screenshot_path)            
         
     elif "donaldjtrump"  in url:
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Enter your Email']").send_keys(df['email'][i])
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Zip']").send_keys(int(df['zip'][i]))
+        time.sleep(1)
         driver.find_element(By.XPATH,"//input[@placeholder='Zip']").send_keys(Keys.ENTER)
         time.sleep(5)
         
--- vpn_automate.py
+++ vpn_automate.py
@@ -1,115 +1,9 @@
-# import pandas as pd
-# import subprocess
-# import time
-
-# # df = pd.read_csv('state.csv')
-
-# # with open('login.txt', 'r') as f:
-# #     lines = f.readlines()
-# #     username = lines[0].strip()
-# #     password = lines[1].strip()
-
-# def login_vpn(username, password):
-#     # Full path to piactl.exe
-#     piactl_path = r"C:\Program Files\Private Internet Access"
-    
-#     # Example command to login
-#     login_command = [".\\piactl", "login", "--username", username, "--password", password]
-    
-#     # Run the command
-#     result = subprocess.run(login_command, capture_output=True, text=True)
-    
-#     if result.returncode == 0:
-#         print("VPN login successful")
-#     else:
-#         print(f"Error: {result.stderr}")
-
-def get_vpn_ip():
-    result = subprocess.run(['curl', 'https://ipinfo.io/ip'], capture_output=True, text=True)
-    if result.returncode == 0:
-        return result.stdout.strip()
-    else:
-        return "Unable to retrieve IP address"
-
-# def check_vpn_status():
-#     result = subprocess.run(['piactl', 'get', 'connectionstate'], capture_output=True, text=True)
-#     return result.stdout.strip()
-
-# # def connect_vpn(state):
-
-# #     state_to_server = {
-# #         'California': 'us-west',
-# #         'New York': 'us-east',
-# #         # TODO: Add more states
-# #     }
-    
-# #     server = state_to_server.get(state, None)
-# #     if server:
-# #         disconnect_command = ['piactl', 'disconnect']
-# #         subprocess.run(disconnect_command)
-# #         print(f"Disconnected from any active VPN session.")
-        
-# #         while check_vpn_status() != 'Disconnected':
-# #             print("Waiting for VPN to disconnect...")
-# #             time.sleep(2)
-        
-# #         # Set the new server region
-# #         set_server_command = ['piactl', 'set', 'region', server]
-# #         subprocess.run(set_server_command)
-# #         print(f"Set the region to {server} for {state}.")
-        
-# #         # Initiate the connection
-# #         connect_command = ['piactl', 'connect']
-# #         subprocess.run(connect_command)
-# #         print(f"Attempting to connect to the VPN server in {state} ({server})...")
-        
-# #         # Wait until the VPN is fully connected
-# #         connection_attempts = 0
-# #         max_attempts = 10
-# #         while check_vpn_status() != 'Connected' and connection_attempts < max_attempts:
-# #             print("Waiting for VPN to connect...")
-# #             time.sleep(2) 
-# #             connection_attempts += 1
-        
-# #         if check_vpn_status() == 'Connected':
-# #             vpn_ip = get_vpn_ip()
-# #             print(f"Connected successfully. VPN IP Address: {vpn_ip}")
-# #         else:
-# #             print(f"Failed to connect to {server} after {max_attempts} attempts.")
-# #     else:
-# #         print(f"No server found for state: {state}")
-
-# def change_vpn(state, server):
-#     if check_vpn_status() != 'Connected' or get_vpn_ip() != server:
-#         connect_command = ['piactl', 'connect', server]
-#         subprocess.run(connect_command)
-#         print(f"Connecting to VPN for {state} ({server})...")
-
-#         # Wait until VPN is connected
-#         attempts = 0
-#         max_attempts = 5
-#         while check_vpn_status() != 'Connected' and attempts < max_attempts:
-#             print("Waiting for VPN to connect...")
-#             time.sleep(2)
-#             attempts += 1
-
-#         if check_vpn_status() == 'Connected':
-#             vpn_ip = get_vpn_ip()
-#             print(f"Connected successfully. VPN IP: {vpn_ip}")
-#             return vpn_ip
-#         else:
-#             print(f"Failed to connect to {server} after {max_attempts} attempts.")
-#             return None
-#     else:
-#         return get_vpn_ip()
-
 import subprocess
 import time
 
-# Provide the full path to piactl
 PIA_PATH = r'C:\Program Files\Private Internet Access\piactl.exe'
 
-# Function to log in to the VPN using username and password
+# Log in to the VPN using username and password
 def login_vpn(username, password):
     login_command = [PIA_PATH, 'login', '--username', username, '--password', password]
     result = subprocess.run(login_command, capture_output=True, text=True)
@@ -118,17 +12,20 @@ def login_vpn(username, password):
     else:
         print(f"Failed to log in: {result.stderr}")
 
-# Function to check VPN connection status
+# Check VPN connection status
 def check_vpn_status():
     result = subprocess.run([PIA_PATH, "get", "connectionstate"], capture_output=True, text=True)
     return result.stdout.strip()
 
-# Function to get the current VPN IP
-# def get_vpn_ip():
-#     result = subprocess.run([PIA_PATH, "get", "ip"], capture_output=True, text=True)
-#     return result.stdout.strip()
-
-# Function to change VPN based on the pia_vpn value from CSV
+# Get the VPN IP
+def get_vpn_ip():
+    result = subprocess.run(['curl', 'https://ipinfo.io/ip'], capture_output=True, text=True)
+    if result.returncode == 0:
+        return result.stdout.strip()
+    else:
+        return "Unable to retrieve IP address"
+    
+# Change VPN based on the pia_vpn value from CSV
 def change_vpn(pia_vpn):
     if check_vpn_status() != 'Connected' or get_vpn_ip() != pia_vpn:
         # Step 1: Set the region
